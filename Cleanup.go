cd71499e3490aca031b8be277c4b3ad1771b7a42
PrivateGER <privateger@privateger.me>
2022-10-07 04:32:19 +0200

    Add grouping of PHash results

--- Cleanup.go
+++ Cleanup.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"Paktum/Database"
 	"github.com/corona10/goimagehash"
 	"github.com/meilisearch/meilisearch-go"
 	log "github.com/sirupsen/logrus"
+	"time"
 )
 
 func CleanupMode(client *meilisearch.Client) {
@@ -17,6 +19,10 @@ func CleanupMode(client *meilisearch.Client) {
 	}
 
 	log.Info("Got ", len(docs.Results), " documents from MeiliSearch")
+
+	startTime := time.Now()
+
+	duplicates := make(map[string][]Database.PHashEntry)
 
 	// find duplicates using pHash
 	for i, doc := range docs.Results {
@@ -49,7 +55,95 @@ func CleanupMode(client *meilisearch.Client) {
 
 			if distance < 10 {
 				log.Info("Found possible image duplicate with IDs ", needleID, " and ", otherID, " with distance ", distance)
+				duplicates[needleID] = append(duplicates[needleID], Database.PHashEntry{
+					ID:       otherID,
+					Hash:     uint64(otherHash),
+					Distance: distance,
+				})
 			}
 		}
 	}
+
+	var duplicateGroups [][]Database.PHashEntry
+
+	// detect duplicate groups and add them to a nested array to have a simplified way to create graphs
+	for originalKey, original := range duplicates {
+		// check whether the key of this group exists in the groups array
+		// if not, we create a new group and throw all sub-keys into it along with the main key
+		// if it does, we add all sub-keys to the group, including the main key
+
+		// find the index of the group where the original key is a member
+		// if it is not a member of any group, it returns -1
+		groupIndex := FindInside([]Database.PHashEntry{FindPHashFromID(originalKey, docs)}, duplicateGroups)
+		if groupIndex == -1 {
+			// create a new group
+			duplicateGroups = append(duplicateGroups, append(original, FindPHashFromID(originalKey, docs)))
+		} else {
+			// add the original key to the group
+			duplicateGroups[groupIndex] = append(duplicateGroups[groupIndex], FindPHashFromID(originalKey, docs))
+
+			// add all sub-keys to the groups
+			duplicateGroups[groupIndex] = MergeGroups(duplicateGroups[groupIndex], original)
+		}
+	}
+
+	log.Info("Found ", len(duplicateGroups), " duplicate groups")
+	for i, group := range duplicateGroups {
+		log.Info("Group ", i, " contains ", len(group), " members")
+		for _, member := range group {
+			log.Info("Member ", member)
+		}
+	}
+
+	log.Info("Finished in ", time.Since(startTime))
+}
+
+func PHashExistsInGroup(hash uint64, group []Database.PHashEntry) bool {
+	for _, member := range group {
+		if member.Hash == hash {
+			return true
+		}
+	}
+
+	return false
+}
+
+func FindInside(haystack []Database.PHashEntry, groups [][]Database.PHashEntry) int {
+	// goes through all groups and detects group where any in haystack is a member, if one is found, it returns the index of the group
+	// if none is found, it returns -1
+	for i, group := range groups {
+		for _, member := range group {
+			for _, needle := range haystack {
+				if member.Hash == needle.Hash {
+					return i
+				}
+			}
+		}
+	}
+
+	return -1
+}
+
+func MergeGroups(originalGroup []Database.PHashEntry, newGroup []Database.PHashEntry) []Database.PHashEntry {
+	for _, member := range newGroup {
+		if !PHashExistsInGroup(member.Hash, originalGroup) {
+			originalGroup = append(originalGroup, member)
+		}
+	}
+
+	return originalGroup
+}
+
+func FindPHashFromID(id string, docs meilisearch.DocumentsResult) Database.PHashEntry {
+	for _, doc := range docs.Results {
+		if doc["ID"] == id {
+			return Database.PHashEntry{
+				ID:       id,
+				Hash:     uint64(doc["PHash"].(float64)),
+				Distance: 0,
+			}
+		}
+	}
+
+	return Database.PHashEntry{}
 }
